d6b8eb01cfc648b828b35a7caf241e373ed1e4be
Yeryomin1 <48363433+Yeryomin1@users.noreply.github.com>
2020-12-18 18:37:27 +0300

    surface added

--- js/main.js
+++ js/main.js
@@ -3,19 +3,38 @@ window.onload = function () {
     class External {
         constructor(threeScene, objNum) {
             this.objects = new THREE.Object3D();
+
+            let material = new THREE.MeshPhongMaterial({
+                color: 0xdaa520,
+                specular: 0xbcbcbc,
+                side: THREE.DoubleSide,
+            });
             for (let i = 0; i < objNum; i++) {
-                let geometry = new THREE.SphereGeometry(10, 1, 1);
-                let material = new THREE.MeshPhongMaterial({
-                    color: 'Gray',
-                    specular: 0xbcbcbc,
-                });
-                let particle = new THREE.Mesh(geometry, material);
-                particle.position.x = (Math.random() - 0.5) * 500;
-                particle.position.y = (Math.random() - 0.5) * 500;
-                particle.position.z = -1000 + (Math.random() - 0.5) * 1000;
-                this.objects.add(particle);
+                let geometry = new THREE.SphereGeometry(Math.random() * 20 + 1, Math.random() * 4 + 4, Math.random() * 4 + 4);
+
+                let item = new THREE.Mesh(geometry, material);
+                item.position.x = (Math.random() - 0.5) * 1000;
+                item.position.y = (Math.random() - 0.5) * 200;
+                item.position.z = -5000 + Math.random() * 4990;
+                item.rotation.z = Math.random() * Math.PI;
+                item.rotation.y = Math.random() * Math.PI;
+                this.objects.add(item);
             }
-            threeScene.add(this.objects);
+
+            let planeSize = 10000;
+            let geometry = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+            let surface = new THREE.Mesh(geometry, material);
+            surface.rotation.x = Math.PI * -.5;
+            surface.position.set(0, -150, 0);
+            this.objects.add(surface);
+
+
+            this.pivot = new THREE.Object3D();
+            this.pivot.add(this.objects);
+
+
+            threeScene.add(this.pivot);
+
         }
     }
 
@@ -47,24 +66,23 @@ window.onload = function () {
         renderer = new THREE.WebGLRenderer({ canvas: canvas });
         renderer.setClearColor(0x000000);
         scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
+        camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 2000);
         camera.position.set(0, 0, 1);
         camera.lookAt(0, 0, -1);
-        //создаем свет, заливающий белый
-        let light = new THREE.AmbientLight(0x404040);
-        scene.add(light);
-        let spotLight = new THREE.SpotLight(0xeeee00);
-        spotLight.position.set(1000, 1000, -1000);
-        //spotLight.position.set(0, 0, -200);
+
+        //создаем свет
+        let spotLight = new THREE.SpotLight(0xffffff);
+        spotLight.position.set(0, 0, 1);
+
         scene.add(spotLight);
         //туман
-        scene.fog = new THREE.Fog(0x000000, 0.01);
+        scene.fog = new THREE.Fog(0x000000, 2, 10000);
         //таймер
         vehicle.time = new Date();
         //искусственный горизонт
         vehicle.attitude = new Attitude(camera, scene, 0.25, 70, 70);
         //внешние объекты
-        vehicle.external = new External(scene, 200);
+        vehicle.external = new External(scene, 20);
     }
 
     vehicle.update = function () {
@@ -97,12 +115,13 @@ window.onload = function () {
         vehicle.update();
         //поворот пространства по тангажу:
         let axisOfRotation = new THREE.Vector3(1, 0, 0);
-        vehicle.external.objects.rotateOnAxis(axisOfRotation, -vehicle.dPitch * Math.PI / 180);
+        vehicle.external.pivot.rotateOnAxis(axisOfRotation, -vehicle.dPitch * Math.PI / 180);
         //поворот пространства по крену:
         axisOfRotation = new THREE.Vector3(0, -Math.sin(vehicle.pitch * Math.PI / 180), Math.cos(vehicle.pitch * Math.PI / 180));
-        vehicle.external.objects.rotateOnAxis(axisOfRotation, vehicle.dRoll * Math.PI / 180);
+        vehicle.external.pivot.rotateOnAxis(axisOfRotation, vehicle.dRoll * Math.PI / 180);
         //линейное перемещение пространства вдоль оси Z:
-        vehicle.external.objects.position.z += vehicle.elapsed * vehicle.speed;
+        vehicle.external.objects.position.z += vehicle.elapsed * vehicle.speed * Math.cos(vehicle.pitch * Math.PI / 180);
+        vehicle.external.objects.position.y -= vehicle.elapsed * vehicle.speed * Math.sin(vehicle.pitch * Math.PI / 180);
         //рендеринг
         renderer.render(scene, camera);
         //рекурсивная анимация
